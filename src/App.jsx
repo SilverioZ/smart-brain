2a4d98647b149b06733e621809256d1f55b5d5f9
Silverio Zuniga <silvermoon12286@gmail.com>
2026-02-18 16:08:43 -0600

    back-end URL

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ displayFaceBox = (box) => {
 
 onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input})
-          fetch('http://localhost:3000/imageurl', {
+          fetch('https://smart-brain-api-mvcs.onrender.com/imageurl', {
                  method: 'post',
                  headers: {'Content-Type': 'application/json'},
                  body: JSON.stringify({
@@ -77,7 +77,7 @@ onButtonSubmit = () => {
         .then(response => response.json())
         .then(response => {
             if(response) {
-                fetch('http://localhost:3000/image', {
+                fetch('https://smart-brain-api-mvcs.onrender.com/image', {
                  method: 'put',
                  headers: {'Content-Type': 'application/json'},
                  body: JSON.stringify({
