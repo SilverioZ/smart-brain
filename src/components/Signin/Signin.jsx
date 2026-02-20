9ec653e25377f0b30ac4cea37c3f4357c2dd4da0
SilverioZ <silvermoon12286@gmail.com>
2026-02-20 16:36:47 -0600

    Update Signin component with new API endpoint

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     this.setState({signInPassword: event.target.value})
   }
 	onSubmitSignIn = () => {
-    fetch('http://localhost:3000/api/data/signin', {
+    fetch('http://localhost:5432/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -78,6 +78,7 @@ class Signin extends React.Component {
 
 
 export default Signin;
+
 
 
 
