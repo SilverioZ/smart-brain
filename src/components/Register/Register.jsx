4e0b558993bdc645c4d8fda4be98c499c360ee2d
SilverioZ <silvermoon12286@gmail.com>
2026-02-20 16:45:56 -0600

    Fix register component and update fetch URL

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:${port}/register', {
+    fetch('http://localhost:3000/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -88,6 +88,7 @@ class Register extends React.Component {
 
 
 export default Register;
+
 
 
 
