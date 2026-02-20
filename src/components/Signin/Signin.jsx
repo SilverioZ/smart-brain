b6170d4b34da087340961876f76dd24dd8b611b2
SilverioZ <silvermoon12286@gmail.com>
2026-02-20 17:06:10 -0600

    Update Signin.jsx

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Signin.css'
-
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3000'
 
 class Signin extends React.Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     this.setState({signInPassword: event.target.value})
   }
 	onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch(`${BACKEND_URL}/api/data`/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -78,6 +78,7 @@ class Signin extends React.Component {
 
 
 export default Signin;
+
 
 
 
